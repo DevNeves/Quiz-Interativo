846c12a9f1a1d5a15b7407f1f66b386bb4be7400
Vitor Neves <102633306+DevNeves@users.noreply.github.com>
2023-05-10 20:33:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+
+const form = document.querySelector('form')
+const showPoints = document.querySelector('.show-points')
+const correctAsnwers = ['B', 'A', 'B', 'A', 'A']
+
+const asnwers = ( ) =>  {
+    const userAsnwers = [
+    form.inputQuestion1.value,
+    form.inputQuestion2.value,
+    form.inputQuestion3.value,
+    form.inputQuestion4.value,
+    form.inputQuestion5.value,
+    ]
+
+    return userAsnwers
+}
+
+const insertPoints = (text) => {
+    showPoints.textContent = `${text}`
+}
+
+const userCorrectAsnwers = event => {
+    event.preventDefault()
+
+    let points = 0
+
+    const isACorrectAsnwers = (asnwer, i) => {
+        if (asnwer === correctAsnwers[i]){
+           points += 1
+        }
+        
+        insertPoints(`Você acertou ${points}/5 perguntas!`)
+
+        if (points === 0) {
+            insertPoints(`Você acertou nenhuma das 5 perguntas )=`)
+        }
+    }
+
+    asnwers().forEach(isACorrectAsnwers)
+}
+
+form.addEventListener('submit' , userCorrectAsnwers)
